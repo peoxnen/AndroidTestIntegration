6c341f5e174e73cb1e16f9b1800a75b997505601
peoxnen <sienski.witold@gmail.com>
2016-07-07 21:35:59 +0200

    espresso: integration, basic operations

---
+++ app/src/androidTest/java/iview/wsienski/androidtestintegration/espresso/MainActivityTest.java
@@ -0,0 +1,48 @@
+package iview.wsienski.androidtestintegration.espresso;
+
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import iview.wsienski.androidtestintegration.MainActivity;
+import iview.wsienski.androidtestintegration.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Created by Witold Sienski on 07.07.2016.
+ */
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Test
+    public void testButton(){
+        onView(withId(R.id.button))
+                .perform(click())
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testEditText(){
+        onView(withId(getResourceId("edit_query"))).perform(typeText("test1"));
+        onView(withText("test1")).check(matches(isDisplayed()));
+    }
+
+    private static int getResourceId(String s) {
+        Context targetContext = InstrumentationRegistry.getTargetContext();
+        String packageName = targetContext.getPackageName();
+        return targetContext.getResources().getIdentifier(s, "id", packageName);
+    }
+}
